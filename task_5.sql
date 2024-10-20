9b0e4dd2e47faab1e8931a4f825fab228f7883ee
davembalo <dsmbalo@gmail.com>
2024-10-21 00:16:51 +0200

    Modified task_5 file

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,12 @@
 USE alx_book_store;
 
+CREATE TABLE customer (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT,
+);
+
 INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', cbaidoo@sandtech.com, '123 Happiness Ave');
 
